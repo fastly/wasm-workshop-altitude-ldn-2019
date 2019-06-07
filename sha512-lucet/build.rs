33453acd7c9d79f3d6eab52b8d4150ba59c277a6
Adam C. Foltzer <acfoltzer@fastly.com>
2019-06-06 17:19:55 -0700

    factor out solution files, clean up Rust app more

--- sha512-lucet/build.rs
+++ sha512-lucet/build.rs
@@ -1,10 +1,11 @@
+use failure::Error;
 use lucetc::{Bindings, Lucetc, LucetcOpts};
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+fn main() -> Result<(), Error> {
+    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
 
     let wasm_path = cargo_manifest_dir
         .parent()
@@ -14,8 +15,11 @@ fn main() {
         .join("assembly")
         .join("module.wasm");
 
+    let bindings = Bindings::from_file(cargo_manifest_dir.join("bindings.json"))?;
+
     Lucetc::new(&wasm_path)
-        .with_bindings(Bindings::from_file(cargo_manifest_dir.join("bindings.json")).unwrap())
-        .shared_object_file(out_dir.join("module.so"))
-        .unwrap();
+        .with_bindings(bindings)
+        .shared_object_file(out_dir.join("module.so"))?;
+
+    Ok(())
 }
