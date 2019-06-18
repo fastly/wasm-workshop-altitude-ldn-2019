42bb67bb8d31c0366e227454cb18043d8d4d5c7d
Adam C. Foltzer <acfoltzer@fastly.com>
2019-06-18 14:02:10 +0100

    fix the script!

--- run_devenv.sh
+++ run_devenv.sh
@@ -1,4 +1,4 @@
 set -e
 set -x
 
-docker run --rm -it -v "$(pwd)":/workshop -p 8080:8080 wasm-workshop:latest bash
+docker run --rm -it -v "$(pwd)":/workshop -p 8080:8080 acfoltzer/wasm-workshop-altitude-ldn-2019:latest bash
